bdb46531c1fd627195d3cc53a8e536b03022cea6
Taynara Sene <tpereira@tray.net.br>
2021-03-10 13:40:00 -0300

    remove es6

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -2,15 +2,13 @@
 //= require ./app/_search
 
 
-window.addEventListener('load', ()=>{
-    document.querySelectorAll('h2')
-    .forEach((h2) => {
-        title = h2.innerHTML.split('#')[0];
-        tag = `<span class="tag tag--${h2.innerHTML.split('#')[1]}">
-                 ${h2.innerHTML.split('#')[1]} 
-               </span> ${title}` ;
-
-        template = h2.innerHTML.split('#')[1] ? tag: title;
-        h2.innerHTML = template ;
+window.addEventListener('load', () => {
+    document.querySelectorAll('h2').forEach(h2 => {
+      title = h2.innerHTML.split('#')[0];
+      tag = `<span class="tag tag--${h2.innerHTML.split('#')[1]}">
+                   ${h2.innerHTML.split('#')[1]} 
+                 </span> ${title}`;
+      template = h2.innerHTML.split('#')[1] ? tag : title;
+      h2.innerHTML = template;
     });
-})
+  });
